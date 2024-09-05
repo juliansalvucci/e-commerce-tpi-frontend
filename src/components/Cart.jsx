c49a17b159e64d12fb7c4aa8554a40450f3ce600
JaaF97 <julianfassetta@gmail.com>
2024-09-05 02:52:25 -0300

    feat: Componente Cart

---
+++ src/components/Cart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Cart.css";
+
+const Cart = () => {
+  return (
+    <div className=".cart-section">
+      {/* aca iría el ícono del carrito, por ahora "iCart" para ilustrar */}
+      <span>iCart</span>
+    </div>
+  );
+};
+
+export default Cart;
