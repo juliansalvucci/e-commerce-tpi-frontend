f6003dccf3f3f1efec26399f403f739ef459f10e
JaaF97 <julianfassetta@gmail.com>
2024-09-10 10:52:07 -0300

    refactor: Quitar estilo de .app para integrarlo en index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import "./styles/App.css";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Home />
-      <div className="app">
+      <div className="background">
         <h1>APLICACIÓN ECOMMERCE</h1>
       </div>
     </>
