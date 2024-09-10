d17ddccd845db6df88174f8c459377b0a2f1ff5a
JaaF97 <julianfassetta@gmail.com>
2024-09-10 11:04:08 -0300

    refactor: Nueva navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,21 @@ import SearchBar from "./SearchBar";
 import User from "./User";
 import Cart from "./Cart";
 import Logo from "./Logo";
+import Combobox from "./Combobox";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <Logo />
+      <div className="combobox-container">
+        <Combobox title="Categories" />
+        <Combobox title="Promotions" />
+        <Combobox title="Help" />
+        <Combobox title="About" />
+      </div>
       <SearchBar />
-      <User />
       <Cart />
+      <User />
     </nav>
   );
 };
