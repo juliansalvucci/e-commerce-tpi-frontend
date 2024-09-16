76822f98a234bdc6bf6f6308f1af870b369d6d38
JaaF97 <julianfassetta@gmail.com>
2024-09-16 16:13:25 -0300

    feat(ABMCategoryPage): conectar con API (comentado)

--- src/pages/ABMCategoryPage.jsx
+++ src/pages/ABMCategoryPage.jsx
@@ -1,14 +1,32 @@
-import { Form, Formik } from "formik";
+import React from "react";
+import { Formik, Form } from "formik";
 import { categorySchema } from "../schemas";
 import ABMInputComponent from "../components/ABMInputComponent";
 import ABMBackButtonComponent from "../components/ABMBackButtonComponent";
+//import axios from "axios";
+import "../styles/ABM.css";
 
-const onSubmit = async (values, actions) => {
-  /*console.log(values);
-    console.log(actions);*/
-  /*Aca iría la llamada a la API para crear la categoría*/
-  console.log("Creando Categoría...");
-  actions.resetForm();
+// Función que se ejecutará al enviar el form
+const onSubmit = async (values, { resetForm }) => {
+  /*
+  try {
+    const response = await axios.post(
+      "http://localhost:8080/category",
+      values
+    );
+    console.log("Respuesta del servidor:", response.data);
+    // Aca íria la lógica de mostrar un mensaje de exito
+  } catch (error) {
+    console.error("Error en el registro:", error);
+    // Aca íria la lógica de mostrar el error
+  } finally {
+    setSubmitting(false);
+  }
+  */
+  console.log("Formulario enviado con valores:", values);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  resetForm();
+  alert("Formulario enviado");
 };
 
 const ABMCategoryPage = () => {
@@ -19,28 +37,30 @@ const ABMCategoryPage = () => {
         <h1 className="title">
           Creá una Categoría
           <Formik
-            initialValues={{ nombre: "", descripcion: "" }}
-            validationSchema={categorySchema}
-            onSubmit={onSubmit}
+            initialValues={{ nombre: "", descripcion: "" }} // Valores iniciales del formulario
+            validationSchema={categorySchema} // Esquema de validación
+            onSubmit={onSubmit} // Función al enviar el formulario
           >
             {({ isSubmitting }) => (
               <Form>
                 <ABMInputComponent
                   label="NOMBRE"
+                  id="nombre"
                   name="nombre"
                   type="text"
                   placeholder="Ingrese el nombre"
                 />
                 <ABMInputComponent
                   label="DESCRIPCIÓN"
+                  id="descripcion"
                   name="descripcion"
                   type="text"
                   placeholder="Ingrese la descripción"
                 />
                 <button
                   className="btn-crear"
-                  disabled={isSubmitting}
                   type="submit"
+                  disabled={isSubmitting}
                 >
                   Crear
                 </button>
@@ -52,4 +72,5 @@ const ABMCategoryPage = () => {
     </div>
   );
 };
+
 export default ABMCategoryPage;
