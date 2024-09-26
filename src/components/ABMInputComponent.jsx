99823a0471f94aa649179e06c16ce864e09709b4
JaaF97 <julianfassetta@gmail.com>
2024-09-26 00:10:28 -0300

    refactor(ABMInput): quitar codigo innecesario

--- src/components/ABMInputComponent.jsx
+++ src/components/ABMInputComponent.jsx
@@ -1,26 +1,8 @@
 import React from "react";
-import { useEffect } from "react";
-import { useField, useFormikContext } from "formik";
-
-const formatPrice = (value) => {
-  if (!value) return "";
-  const number = value.replace(/\D/g, ""); // Elimina cualquier caracter que no sea número
-  return "$" + new Intl.NumberFormat().format(number); // Formatea el número con comas
-};
+import { useField } from "formik";
 
 const ABMInputComponent = ({ label, id, ...props }) => {
-  const [field, meta, helpers] = useField(props);
-  const { setValue } = helpers;
-  const { values } = useFormikContext();
-
-  useEffect(() => {
-    if (props.type === "text" && props.name === "precio") {
-      const formattedPrice = formatPrice(values[props.name]);
-      if (formattedPrice !== values[props.name]) {
-        setValue(formattedPrice); // Actualiza el valor formateado en el input
-      }
-    }
-  }, [values[props.name], props.name, setValue]);
+  const [field, meta] = useField(props);
 
   return (
     <div className="form-group">
@@ -29,7 +11,10 @@ const ABMInputComponent = ({ label, id, ...props }) => {
         className={meta.touched && meta.error ? "input-error" : ""}
         {...field}
         {...props}
-        id={id} // Esto corrige: The label's for attribute refers to a form field by its name, not its id. This might prevent the browser from correctly autofilling the form and accessibility tools from working correctly.         To fix this issue, refer to form fields by their id attribute.
+        id={id} // Esto corrige: The label's for attribute refers to a form field by its name, 
+        // not its id. This might prevent the browser from correctly autofilling the form and 
+        // accessibility tools from working correctly. To fix this issue, refer to form fields
+        // by their id attribute.
       />
       {meta.touched && meta.error ? (
         <div className="error">{meta.error}</div>
