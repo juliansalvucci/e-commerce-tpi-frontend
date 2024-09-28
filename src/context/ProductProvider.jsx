5b1d6389435b0ba682c509bd46bc316e01efb935
mroja <mrojaslopez03@gmail.com>
2024-09-28 12:46:34 -0300

    feat: actualizar fetch del ProductProvider

--- src/context/ProductProvider.jsx
+++ src/context/ProductProvider.jsx
@@ -1,27 +1,15 @@
 import { useEffect, useState } from "react";
 import { ProductContext } from "./ProductContext";
 import Swal from "sweetalert2";
+import axios from "axios";
 
 export const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
 
   const fetchProducts = async () => {
     try {
-      const response = await fetch("https://fakestoreapi.in/api/products");
-      const data = await response.json();
-
-      console.log("Productos recibidos:", data); // Revisa la estructura de la respuesta
-
-      // Verifica si `data` es un array o un objeto
-      if (Array.isArray(data)) {
-        // Si `data` es directamente un array de productos
-        setProducts(data);
-      } else if (data.products) {
-        // Si `data` es un objeto con `products` anidado
-        setProducts(data.products);
-      } else {
-        throw new Error("La respuesta de la API no contiene productos");
-      }
+      const response = await axios.get("https://fakestoreapi.in/api/products");
+      setProducts(response);
     } catch (error) {
       Swal.fire({
         icon: 'error',
