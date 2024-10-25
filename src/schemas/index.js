b606e59cfbffa73fc7bb99152886b669294cac5e
JaaF97 <julianfassetta@gmail.com>
2024-10-25 03:36:25 -0300

    feat: actualizar validaciones de color de producto. #47

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,6 +3,7 @@ import * as yup from "yup";
 const alMenosUnaLetra = /[a-zA-Z]/;
 const primerCharacterLetraONumero = /^[a-zA-Z0-9]/;
 const primerCharacterLetra = /^[a-zA-Z]/;
+const soloLetras = /^[a-zA-Z]+$/;
 
 export const brandSchema = yup.object().shape({
   nombre: yup
@@ -49,10 +50,16 @@ export const productSchema = yup.object().shape({
       "El primer caracter debe ser una letra o un número"
     )
     .matches(alMenosUnaLetra, "El nombre debe contener al menos una letra"),
-  descripcion: yup
+  color: yup
     .string()
     .required("Obligatorio")
-    .max(100, "Maximo 100 caracteres"),
+    .min(3, "El color debe contener al menos 2 caracteres")
+    .max(20, "El color debe tener como maximo 20 caracteres")
+    .matches(soloLetras, "El color debe contener solo letras"),
+  tamaño: yup.string().notRequired(),
+  marca: yup.string().required("Obligatorio"),
+  categoria: yup.string().required("Obligatorio"),
+  subcategoria: yup.string().required("Obligatorio"),
   precio: yup
     .number()
     .positive("El precio debe ser mayor a $0")
@@ -66,10 +73,10 @@ export const productSchema = yup.object().shape({
     .min(0, "El stock minimo debe ser mayor o igual a 0")
     .required("Obligatorio"),
   imagen: yup.string().notRequired().url("Debe ser una URL válida"),
-  marca: yup.string().required("Obligatorio"),
-  subcategoria: yup.string().required("Obligatorio"),
-  color: yup.string().notRequired(),
-  tamaño: yup.string().notRequired(),
+  descripcion: yup
+    .string()
+    .required("Obligatorio")
+    .max(100, "Maximo 100 caracteres"),
 });
 
 export const registerSchema = yup.object().shape({
