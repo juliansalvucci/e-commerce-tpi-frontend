4367f3553f4d40ee452108139edad4bb344370a8
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2024-10-30 17:19:11 -0300

    chore: Remove unused import axios

--- src/context/BrandProvider.jsx
+++ src/context/BrandProvider.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import axios from "axios";
 import Swal from "sweetalert2";
 import { BrandContext } from "./BrandContext";
 import formatDateTime from "../utils/formatDateTimeUtils";
