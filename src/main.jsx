d8eb9c10136c46453f87eb42f0cc876f2642eeca
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2024-10-30 17:53:21 -0300

    chore: restore main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { CarritoApp } from "./CarritoApp";
 import { BrowserRouter } from "react-router-dom";
-import { RegisterPage } from "./pages/RegisterPage";
-import { UserProvider } from "./context/UserProvider";
-import { LoginPage } from "./pages/LoginPage";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
       <CarritoApp />
-      <UserProvider>
-        <RegisterPage />
-        <LoginPage/>
-      </UserProvider>
     </StrictMode>
-    ,
   </BrowserRouter>
 );
