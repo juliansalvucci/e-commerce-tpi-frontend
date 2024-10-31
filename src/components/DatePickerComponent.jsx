fc4e1890cfd8aad6062c85c76176f406420820f9
JaaF97 <julianfassetta@gmail.com>
2024-10-31 00:23:07 -0300

    refactor: refactorizar DatePicker con funciones no obsoletas

--- src/components/DatePickerComponent.jsx
+++ src/components/DatePickerComponent.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { useField, useFormikContext } from "formik";
-import { DatePicker } from "@mui/x-date-pickers";
-import TextField from "@mui/material/TextField";
-import { LocalizationProvider } from "@mui/x-date-pickers";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 const DatePickerComponent = ({ label, fullWidth, ...props }) => {
@@ -12,25 +10,18 @@ const DatePickerComponent = ({ label, fullWidth, ...props }) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <DatePicker
-        label={label}
-        value={field.value || null} //Valor del campo de fecha
-        onChange={(value) => setFieldValue(field.name, value)} // Actualizar el valor en Formik cuando cambia el DatePicker
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            {...field}
-            fullWidth={fullWidth} // Prop fullWidth para controlar el ancho
-            error={meta.touched && Boolean(meta.error)}
-            helperText={meta.touched && meta.error ? meta.error : ""}
-            sx={{
-              maxWidth: "1500px", // Ajuste del ancho similar al ABMInputComponent
-              "& .MuiInputBase-root": {
-                backgroundColor: "white",
-              },
-            }}
-          />
-        )}
         {...props}
+        label={label}
+        value={field.value || null}
+        onChange={(value) => setFieldValue(field.name, value)} // Actualizar el valor en Formik cuando cambia el DatePicker
+        error={meta.touched && Boolean(meta.error)}
+        helperText={meta.touched && meta.error ? meta.error : ""}
+        sx={{
+          maxWidth: "1500px", // Ajuste del ancho similar al ABMInputComponent
+          "& .MuiInputBase-root": {
+            backgroundColor: "white",
+          },
+        }}
       />
     </LocalizationProvider>
   );
