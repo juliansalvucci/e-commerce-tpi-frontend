c3abcbb38c579f0def10cb65c4b591cfe688e16c
JaaF97 <julianfassetta@gmail.com>
2024-12-09 19:33:54 -0300

    feat: agregar autenticación a endpoints requeridos. #101

--- src/api/api.js
+++ src/api/api.js
@@ -1,32 +1,66 @@
-// api.js
 import axios from "axios";
 
 // Crea una instancia de Axios
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "/api",
 });
+
+// Define una función para verificar si un endpoint requiere JWT
+const requiereJWT = (method, url) => {
+  const rutasRequierenJWT = [
+    // User
+    { method: "GET", url: "/user" },
+    { method: "PUT", url: "/user" },
+    { method: "DELETE", url: "/user" },
+    { method: "POST", url: "/user/recover" },
+
+    // Subcategory
+    { method: "PUT", url: "/subcategory" },
+    { method: "DELETE", url: "/subcategory" },
+    { method: "POST", url: "/subcategory" },
+    { method: "POST", url: "/subcategory/recover" },
+
+    // Product
+    { method: "PUT", url: "/product" },
+    { method: "DELETE", url: "/product" },
+    { method: "POST", url: "/product" },
+    { method: "POST", url: "/product/recover" },
+    { method: "PATCH", url: "/product/update-stock" },
+
+    // Category
+    { method: "PUT", url: "/category" },
+    { method: "DELETE", url: "/category" },
+    { method: "POST", url: "/category" },
+    { method: "POST", url: "/category/recover" },
+
+    // Brand
+    { method: "PUT", url: "/brand" },
+    { method: "DELETE", url: "/brand" },
+    { method: "POST", url: "/brand" },
+    { method: "POST", url: "/brand/recover" },
+
+    // Stock-Entry
+    { method: "POST", url: "/stock-entry" },
+
+    // Orders
+    { method: "POST", url: "/orders" },
+  ];
+
+  // Verificar si el método y la URL coinciden con alguna de las rutas que requieren JWT
+  return rutasRequierenJWT.some(
+    (ruta) => ruta.method === method && ruta.url === url
+  );
+};
 
 // Agrega un interceptor para incluir el token en cada petición
 api.interceptors.request.use(
   (config) => {
-    // Verifica si la ruta requiere un token
-    const isAuthRoute = config.url.includes("/auth/signin");
-    const isRegisterRoute = config.url.includes("/auth/signup");
-    const isProductListRoute = config.url.includes("/product");
-    const isUserListRouter = config.url.includes("/user");
-    const isOrderListRouter = config.url.includes("/orders");
-
-    if (
-      !isAuthRoute &&
-      !isRegisterRoute &&
-      !isProductListRoute &&
-      !isUserListRouter &&
-      !isOrderListRouter
-    ) {
-      const token = sessionStorage.getItem("token"); // Token del local storage
+    // Si el endpoint requiere JWT, añade el token
+    if (requiereJWT(config.method.toUpperCase(), config.url)) {
+      const token = sessionStorage.getItem("token");
 
       if (token) {
-        config.headers.Authorization = `Bearer ${token}`; // Incluye el token en los encabezados de la petición
+        config.headers.Authorization = `Bearer ${token}`; // Incluye el token en los headers
       }
     }
 
