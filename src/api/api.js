68b52954cee9876dee7837ebf4b31eaabf9b833b
JaaF97 <julianfassetta@gmail.com>
2024-12-10 01:40:29 -0300

    refactor: inversión de condición de interceptor requiereJWT()

--- src/api/api.js
+++ src/api/api.js
@@ -7,56 +7,31 @@ const api = axios.create({
 
 // Define una función para verificar si un endpoint requiere JWT
 const requiereJWT = (method, url) => {
-  const rutasRequierenJWT = [
-    // User
-    { method: "GET", url: "/user" },
-    { method: "PUT", url: "/user" },
-    { method: "DELETE", url: "/user" },
-    { method: "POST", url: "/user/recover" },
-
-    // Subcategory
-    { method: "PUT", url: "/subcategory" },
-    { method: "DELETE", url: "/subcategory" },
-    { method: "POST", url: "/subcategory" },
-    { method: "POST", url: "/subcategory/recover" },
-
-    // Product
-    { method: "PUT", url: "/product" },
-    { method: "DELETE", url: "/product" },
-    { method: "POST", url: "/product" },
-    { method: "POST", url: "/product/recover" },
-    { method: "PATCH", url: "/product/update-stock" },
-
-    // Category
-    { method: "PUT", url: "/category" },
-    { method: "DELETE", url: "/category" },
-    { method: "POST", url: "/category" },
-    { method: "POST", url: "/category/recover" },
-
-    // Brand
-    { method: "PUT", url: "/brand" },
-    { method: "DELETE", url: "/brand" },
-    { method: "POST", url: "/brand" },
-    { method: "POST", url: "/brand/recover" },
-
-    // Stock-Entry
-    { method: "POST", url: "/stock-entry" },
-
-    // Orders
-    { method: "POST", url: "/orders" },
+  const rutasNoRequierenJWT = [
+    // Subcategory:
+    { method: "GET", url: "/subcategory" },
+    // Product:
+    { method: "GET", url: "/product" },
+    // Category: 
+    { method: "GET", url: "/category" },
+    // Brand:
+    { method: "GET", url: "/brand" },
   ];
 
   // Verificar si el método y la URL coinciden con alguna de las rutas que requieren JWT
-  return rutasRequierenJWT.some(
-    (ruta) => ruta.method === method && ruta.url === url
+  return rutasNoRequierenJWT.some(
+    (ruta) =>
+      ruta.method === method &&
+      (typeof ruta.url === "string" ? ruta.url === url : ruta.url.test(url))
   );
 };
 
 // Agrega un interceptor para incluir el token en cada petición
 api.interceptors.request.use(
   (config) => {
+    console.log(config.url);
     // Si el endpoint requiere JWT, añade el token
-    if (requiereJWT(config.method.toUpperCase(), config.url)) {
+    if (!requiereJWT(config.method.toUpperCase(), config.url)) {
       const token = sessionStorage.getItem("token");
 
       if (token) {
