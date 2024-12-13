621049f5fbb5b2c3720998f0d86f22e2a999f728
JaaF97 <julianfassetta@gmail.com>
2024-12-13 00:01:09 -0300

    chore: actualizar api target

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080", // URL del backend
+        target: "http://localhost:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
